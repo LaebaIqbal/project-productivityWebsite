3401aea5abe55e195cb72b6b2bf5bea4de4cea2b
Laeba <213860383+LaebaIqbal@users.noreply.github.com>
2026-01-11 17:27:10 +0100

    add useState

--- project-productivityWebsite/src/AppointmentsForm.tsx
+++ project-productivityWebsite/src/AppointmentsForm.tsx
@@ -1,12 +1,45 @@
+import { useState } from "react";
+
+//Type für State (html inputs liefern immer string)
+interface InputType {
+  title: string;
+  date: string;
+  time: string;
+}
+const initialState = { title: "", date: "", time: "" };
 function AppointmentsForm() {
+  //input: aktueller Zustand, setInput: Funktion, die zusatnd ändert
+  //useState<InputType>(initialState): Startwerte;
+  const [input, setInput] = useState<InputType>(initialState);
+
+  //Funktion zum ändern Status
+  //"..."" sonst würde man die andeen inuts verlieren
+  //e.target.value : das was man ins textfeld tippt
+
+  function setTitle(e: any) {
+    setInput({ ...input, title: e.target.value });
+  }
+
+  function setDate(e: any) {
+    setInput({ ...input, date: e.target.value });
+  }
+
+  function setTime(e: any) {
+    setInput({ ...input, time: e.target.value });
+  }
+
+  console.log(input);
+
+  //OnChange: wenn Wert diese Inputs ändert, dann rufe entsprechend eFunktion auf
   return (
     <>
       <h3>Title</h3>
-      <input type="text"></input>
+      <input type="text" onChange={(e) => setTitle(e)}></input>
       <h3>Date</h3>
-      <input type="date"></input>
+      <input type="date" onChange={(e) => setDate(e)}></input>
       <h3>Time</h3>
-      <input type="time"></input>
+      <input type="time" onChange={(e) => setTime(e)}></input>
+      <button>Add Appointment</button>
     </>
   );
 }
